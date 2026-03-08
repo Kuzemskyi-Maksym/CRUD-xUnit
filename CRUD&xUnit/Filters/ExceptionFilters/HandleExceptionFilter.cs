cc08501eb7f4b80f453e91d1293216269908838e
Kuzemskyi-Maksym <maksymkuzemskyi@gmail.com>
2026-03-08 22:34:32 +0200

    Injected IHostEnvironment into ExceptionFilter

--- CRUD&xUnit/Filters/ExceptionFilters/HandleExceptionFilter.cs
+++ CRUD&xUnit/Filters/ExceptionFilters/HandleExceptionFilter.cs
@@ -6,17 +6,22 @@ namespace CRUD_xUnit.Filters.ExceptionFilters
     public class HandleExceptionFilter : IExceptionFilter
     {
         private readonly ILogger<HandleExceptionFilter> _logger;
+        private readonly IHostEnvironment _host;
 
-        public HandleExceptionFilter(ILogger<HandleExceptionFilter> logger)
+        public HandleExceptionFilter(ILogger<HandleExceptionFilter> logger, IHostEnvironment host)
         {
             _logger = logger;
+            _host = host;
         }
         public void OnException(ExceptionContext context)
         {
             _logger.LogError("Exception filter {FilterName}.{MethodName}\n {ExceptionType}\n {ExceptionMessage}", 
                 nameof(HandleExceptionFilter), nameof(OnException), context.Exception.GetType().ToString(), context.Exception.Message);
 
-            context.Result = new ContentResult() { Content = context.Exception.Message, StatusCode = 500};
+            if (_host.IsDevelopment())
+            {
+                context.Result = new ContentResult() { Content = context.Exception.Message, StatusCode = 500 };
+            }
         }
     }
 }
